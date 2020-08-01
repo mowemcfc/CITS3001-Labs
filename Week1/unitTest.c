142426345a6ae3a45f862dd23bb07d3a7b15bfc8
mowemcfc <jcartermcfc@gmail.com>
2020-08-01 13:28:08 +0800

    Support user-defined test size, stronger error reporting

--- Week1/unitTest.c
+++ Week1/unitTest.c
@@ -1,28 +1,46 @@
+#include<stdlib.h>
 #include "../Unity/src/unity.h"
 #include "insertSort.h"
 #include "helpers.h"
 
+
 void setUp(void)
 {
-    //setup stuff
+    //setup stuff - not required
 }
 
 void tearDown(void)
 {
-    //cleanup stuff
+    //cleanup stuff - not required
 }
-
-
 
 void test_function_insertSort(void)
 {
-    printf("hello");
-    int* a5 = generate_random_array(500);
-    printf("hello");
-    int* a5_sorted = insertSort(a5, 500);
-    printf("hello");
+    int numTests = 500; // change this
+    int* sizeArray = generate_random_array(numTests); // generate random sizes for testing of array
 
-    TEST_ASSERT_TRUE_MESSAGE(is_sorted(a5_sorted, 500), "Array a5 is not sorted");
+    int* a;
+    int* a_sorted;
+    int a_len; // size of array for random test
+
+    // Error and test messages
+    char* e_msg = malloc(64);
+    char* t_msg = malloc(64);
+
+    for(int i = 0; i < numTests; i++){
+        sprintf(t_msg, "Starting test %d, length %d", i, a_len);
+        TEST_MESSAGE(t_msg);
+
+        a_len = sizeArray[i];
+        a = generate_random_array(a_len);
+        a_sorted = insertSort(a, a_len);
+
+        sprintf(e_msg, "Test %d failed, exiting", i);
+        TEST_ASSERT_TRUE_MESSAGE(is_sorted(a_sorted, a_len), e_msg);
+    }
+
+    free(e_msg);
+    free(t_msg);
 }
 
 int main(void)
