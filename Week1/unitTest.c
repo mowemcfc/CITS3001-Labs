b00535c6826f84b40f98e7605806f5816826052a
mowemcfc <jcartermcfc@gmail.com>
2020-08-03 02:30:03 +0800

    implemented complete mergesort test and result recording/reporting

--- Week1/unitTest.c
+++ Week1/unitTest.c
@@ -18,12 +18,44 @@ void tearDown(void)
 
 void test_function_mergeSort(void)
 {
+    int num_tests = 500;
+    int* size_array = malloc(num_tests * sizeof(int));
+    populate_random_int_array(&size_array, num_tests);
+    mergeSort(size_array, 0, num_tests-1);
+
     int* a;
     int a_len = 500;
 
-    populate_random_int_array(&a, a_len);
-    mergeSort(a, 0, a_len-1);
+    clock_t start, end;
+    double time_used;
 
+    double** test_results;
+    alloc_results_memory(&test_results, num_tests, 3); // 3 denotes number of columns
+
+    char e_msg[64];
+
+    for(int i = 0; i < num_tests; i++){
+        a_len = size_array[i];
+        a = malloc(a_len * sizeof(int));
+
+        start = clock();
+        mergeSort(a, 0, a_len-1);
+        end = clock();
+
+        time_used = ((double) end - start) / CLOCKS_PER_SEC;
+
+        record_result(&test_results, i, a_len, time_used);
+
+        sprintf(e_msg, "\nTest %d failed, exiting", i); // unity does not support printf string formatting, so pre-definition with sprintf required
+        TEST_ASSERT_TRUE_MESSAGE(is_sorted(a, a_len), e_msg);
+
+        free(a);
+    }
+
+    report_results(test_results, num_tests);
+    free(test_results);
+
+    printf("\n----END MERGE SORT TEST----\n");
     return;
 }
 
@@ -42,7 +74,6 @@ void test_function_insertSort(void)
 
     int* a;
     int a_len;
-    int* a_sorted;
 
     char* e_msg = malloc(64); // error message in case of test fail
 
